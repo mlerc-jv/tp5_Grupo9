443dae3ad9843f8a78dd34a8d270e5006862c41c
antoto17 <tom510betacode@gmail.com>
2026-09-23 21:10:19 -0300

    Clientes y CollectionCliente - Maidana Antonella

--- tp5_grupo9/src/ar/edu/unju/escmi/tp5/dominio/Cliente.java
+++ tp5_grupo9/src/ar/edu/unju/escmi/tp5/dominio/Cliente.java
@@ -1,5 +1,45 @@
 package ar.edu.unju.escmi.tp5.dominio;
 
-public class Cliente {
+public abstract class Cliente {
 
+    private String apellido;
+    private String nombre;
+    private String telefono;
+
+    public Cliente(String apellido, String nombre, String telefono) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Apellido: " + apellido
+                + ", Nombre: " + nombre
+                + ", Telefono: " + telefono;
+    }
 }
